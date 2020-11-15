3970bbb2cecaaac7ed44a40ae9e70cbc0389a483
Alyssa DeMay <camocat11@gmail.com>
2020-11-14 19:44:14 -0600

    Changed connection string

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://Ally:Ally123@cluster0.jxbdf.mongodb.net/test'
+    mongoURI: 'mongodb+srv://Ally:Ally123@cluster0.abidm.mongodb.net/MERN_SHOPPING'
 }
