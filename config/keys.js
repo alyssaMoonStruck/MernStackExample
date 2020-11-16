b05bc2b86983f63ae7838d4544221cbddd3e7e1b
Alyssa DeMay <camocat11@gmail.com>
2020-11-16 14:13:14 -0600

    Jwttoken installed and working

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://Ally:Ally123@cluster0.abidm.mongodb.net/MERN_SHOPPING'
-}
--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require ('path')
-
-const items = require('./routes/api/items');
+const path = require('path')
+const config = require('config')
 
 const app = express();
 
 //Bodyparser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI')
 
 // Connect to Mongo
-mongoose.connect(db,{ useUnifiedTopology: true })
+mongoose.connect(db,{ useUnifiedTopology: true, useCreateIndex: true })
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
 
 //Use Routes 
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'))
+app.use('/api/users', require('./routes/api/users'))
+
 
 // Serve static assests if in production
 if(process.env.NODE_ENV === 'production') {
